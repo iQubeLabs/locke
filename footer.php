1b6f9f2fb12951e4ece28120cf417122e56f9389
Samuel Okoroafor <samuel.i.okoroafor@gmail.com>
2014-04-12 21:25:00 +0100

    Added the header and footer template files and index.php

---
+++ footer.php
@@ -0,0 +1,63 @@
+<!-- Start Footer -->
+<div id="footer" class="section footer-section light-section">
+
+    <a href="http://demo.zoom-arts.com/html/relway/body" id="goTop" class="scroll-to"><i class="icon-arrow-up11"></i></a>
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-12">
+                <!-- Footer Logo -->
+                <div class="logo-img"><img alt="" src="images/dark-logo.png" /></div>
+
+                <!-- Copyright -->
+                <div class="copyright">All rights reserved. Copyright © <?php date('Y'); ?> <a class="accent-color" href="blog.html#">Relway</a></div>
+                <!-- Social Icons -->
+                <div class="social-icons">
+                    <ul>
+                        <li><a href="blog.html#"><i class="icon-facebook"></i></a></li>
+                        <li><a href="blog.html#"><i class="icon-twitter"></i></a></li>
+                        <li><a href="blog.html#"><i class="icon-google-plus"></i></a></li>
+                        <li><a href="blog.html#"><i class="icon-dribbble"></i></a></li>
+                        <li><a href="blog.html#"><i class="icon-linkedin4"></i></a></li>
+                        <li><a href="blog.html#"><i class="icon-instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+<!-- End Footer -->
+
+<!-- Main JS  -->
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+<script type="text/javascript" src="js/jquery.migrate.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/retina-1.1.0.min.js"></script>
+<script type="text/javascript" src="js/jquery.outside-events.min.js"></script>
+<script type="text/javascript" src="js/jquery.easing-1.3.pack.js"></script>
+<script type="text/javascript" src="js/jquery.parallax.js"></script>
+<script type="text/javascript" src="js/initmap.min.js"></script>
+<script type="text/javascript" src="js/jquery.mb.YTPlayer.js"></script>
+<script type="text/javascript" src="js/owl.carousel.min.js"></script>
+<script type="text/javascript" src="twitter/jquery.tweet.min.js"></script>
+<script type="text/javascript" src="js/jquery.appear.js"></script>
+<script type="text/javascript" src="js/jquery.scrollTo.js"></script>
+<script type="text/javascript" src="js/jquery.nav.js"></script>
+<script type="text/javascript" src="js/jquery.fitvids.js"></script>
+<script type="text/javascript" src="js/jquery.isotope.min.js"></script>
+<script type="text/javascript" src="js/counter.js"></script>
+<script type="text/javascript" src="js/imagesloaded.min.js"></script>
+<script type="text/javascript" src="js/smooth-scroll.js"></script>
+<script type="text/javascript" src="js/contact-form.js"></script>
+<script type="text/javascript" src="js/portfolio.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
+
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,168 @@
+<!doctype html>
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><html lang="en" class="no-js"> <![endif]-->
+<html lang="en">
+<head>
+
+    <!-- Basic -->
+    <title>Locke Nigeria | Home</title>
+
+    <!-- Define Charset -->
+    <meta charset="utf-8">
+
+    <!-- Responsive MetaTag -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- Page Description and Author -->
+    <meta name="description" content="Relway - Responsive Parallax One Page Template">
+    <meta name="author" content="ZoOm Arts">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="images/favicon.png">
+
+
+    <!-- Bootstrap CSS Styles -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" media="screen">
+
+    <!-- Plugins CSS Styles -->
+    <link rel="stylesheet" href="css/plugins.css" type="text/css" media="screen">
+
+    <!-- Relway CSS Styles -->
+    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen">
+
+    <!-- Responsive CSS Styles -->
+    <link rel="stylesheet" type="text/css" href="css/responsive.css" media="screen">
+
+    <!-- Css3 Animations Styles -->
+    <link rel="stylesheet" type="text/css" href="css/animate.css" media="screen">
+
+    <!-- Icons Font CSS Styles -->
+    <link rel="stylesheet" type="text/css" href="css/icons.css" media="screen">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" media="screen">
+
+    <!-- Color CSS Styles -->
+    <link rel="stylesheet" type="text/css" href="css/colors/chrome-orange.css" title="chrome-orange" media="screen">
+    <link rel="stylesheet" type="text/css" href="css/colors/yellow.css" title="yellow" media="screen" disabled/>
+    <link rel="stylesheet" type="text/css" href="css/colors/orange.css" title="orange" media="screen" disabled/>
+    <link rel="stylesheet" type="text/css" href="css/colors/red.css" title="red" media="screen" disabled/>
+    <link rel="stylesheet" type="text/css" href="css/colors/pink.css" title="pink" media="screen" disabled/>
+    <link rel="stylesheet" type="text/css" href="css/colors/turquoise.css" title="turquoise" media="screen" disabled/>
+    <link rel="stylesheet" type="text/css" href="css/colors/blue.css" title="blue" media="screen" disabled/>
+    <link rel="stylesheet" type="text/css" href="css/colors/green.css" title="green" media="screen" disabled/>
+
+    <!-- Custom CSS Styles -->
+    <link rel="stylesheet" type="text/css" href="css/custom.css" media="screen">
+
+    <!-- Social CSS Styles -->
+
+    <link rel="stylesheet" type="text/css" href="css/social.css" media="screen">
+
+    <!--[if IE 8]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+
+</head>
+<body>
+
+<!-- Page Loader -->
+<div id="pageloader" class="dark">
+    <div class="loader-container">
+        <div class="loader-logo"><img src="images/loader-logo.png" alt="" /></div>
+        <div class="spinner">
+            <div class="rect1"></div>
+            <div class="rect2"></div>
+            <div class="rect3"></div>
+            <div class="rect4"></div>
+            <div class="rect5"></div>
+        </div>
+    </div>
+</div>
+
+
+
+<!-- Start Home Section -->
+-<div id="home" class="section-home fullscreen one-slide youtube-video dark-section">
+
+    <!-- <div id="clients" class="section clients-section parallax-section dark-section" style="padding-top:50px; padding-bottom:45px; background: url(images/pics/bg-02.jpg);">
+    -->
+    <!-- Overlay -->
+    <div class="section-overlay"></div>
+
+    <!-- Video Background -->
+    <div class="yt-player-bg" data-property="{videoURL:'http://https://www.youtube.com/watch?v=4nnWUoMiPI4',showControls:false,loop:false,containment:'.yt-player-bg',autoPlay:true, mute:false, startAt:0, opacity:1}"></div>
+
+    <!-- Video Controls -->
+    <div class="video-controls">
+        <a class="mute-video"><i class="icon-volume-mute2"></i></a>
+        <a class="play-video"><i class="icon-play5"></i></a>
+    </div>
+
+    <!-- Home Container -->
+    <div class="container text-center">
+        <!-- Some Words -->
+        <p>Our professional services are designed to bring smiles to everyone who needs a home</p>
+        <!-- Text Slider -->
+        <div class="text-slider">
+            <div class="item">
+                <h1 class="fit-text">Welcome To <strong class="accent-color">Locke Nigeria</strong></h1>
+            </div>
+            <div class="item">
+                <h1 class="fit-text">We <strong class="accent-color">Love</strong> Homes</h1>
+            </div>
+            <div class="item">
+                <h1 class="fit-text">We Are <strong class="accent-color">Creative</strong></h1>
+            </div>
+        </div>
+        <!-- Button -->
+        <a href="#about" class="btn-sys btn-light btn-border btn-large btn-crv scroll-to"> <strong>Find Out More about Us! </strong><i class="icon-arrow-right2"></i></a>
+    </div>
+
+</div>
+<!-- End Home Section -->
+
+
+
+<!-- Start Header -->
+<header id="header" class="header-2 dark-header">
+
+    <div class="container">
+
+        <!-- Logo -->
+        <div id="logo">
+            <a href="index.html#"><img alt="" src="images/dark-logo.png" /></a>
+        </div>
+
+        <!-- Toggle Menu - Responsive -->
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#nav-menu"><i class="icon-menu2"></i></button>
+
+        <!-- Navigation Menu -->
+        <div id="nav-menu" class="nav-menu collapse navbar-collapse">
+            <ul>
+                <li class="active"><a href="index.html#home">Home</a></li>
+                <li><a href="index.html#about">About</a></li>
+                <li><a href="index.html#services">Services</a></li>
+                <li><a href="index.html#portfolio">Portfolio</a></li>
+                <li><a href="index.html#skills">Process</a></li>
+                <li><a href="index.html#team">Team</a></li>
+                <!-- <li class="drop">
+                    <a href="index.html#">Features</a>
+                    <ul class="dropdown">
+                        <li><a href="elements.html">Elements</a></li>
+                        <li><a href="typography.html">Typography</a></li>
+                        <li><a href="font-icons.html">Font Icons</a></li>
+                    </ul>
+                </li>
+--> <!--                <li class="drop">
+                    <a href="index.html#">Blog</a>
+                    <ul class="dropdown">
+                        <li><a href="blog-masonry.html">Blog Masonry</a></li>
+                        <li><a href="blog.html">Blog Standard</a></li>
+                    </ul>
+                </li>
+-->                <li><a href="index.html#contact">Contact</a></li>
+            </ul>
+        </div>
+
+    </div>
+
+</header>
+<!-- End Header -->
